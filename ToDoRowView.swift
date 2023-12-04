bcc9660267f0b1e5ba57dd250468baf6c327cfb1
Ethan Garden <ethangard@hotmail.com>
2023-12-04 15:15:35 +0100

    To Do List :)

---
+++ ToDoRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoRowView.swift
+//  EthanGardenApp
+//
+//  Created by Ethan Garden on 2023-11-23.
+//
+
+import SwiftUI
+
+struct ToDoRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoRowView()
+}
